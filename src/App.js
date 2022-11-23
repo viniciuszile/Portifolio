bc6ad541b6cf11b9117cc0449d1598a9bfd45ce9
zile <89661612+viniciuszile@users.noreply.github.com>
2022-11-23 16:51:03 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Components/Header/Header';
+import Sobre from './Components/Sobre/Sobre';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Sobre/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css"
+
+function Header (){
+    return(
+        <div className="container_header">
+                <h1>Sobre</h1>
+                <h1>Skills</h1>
+                <h1>Projetos</h1>
+                <h1>Contato</h1>
+        </div>
+    )
+}
+
+export default Header
